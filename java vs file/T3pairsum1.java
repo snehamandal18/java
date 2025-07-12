ba1e1053160f32b80efd0a794cb897c3f89cf3f8
Sneha Mandal <144563264+snehamandal18@users.noreply.github.com>
2025-07-13 01:21:35 +0530

    Add files via upload

---
+++ java vs file/T3pairsum1.java
@@ -0,0 +1,40 @@
+
+import java.util.*;
+public class T3pairsum1{
+    public static boolean pairsum1(ArrayList<Integer>list, int target )
+    {
+        int lp=0;
+        int rp=list.size()-1;
+        while(lp!=rp)
+        {   //case1
+            if(list.get(lp)+list.get(rp)==target){
+             return true;
+            }
+            //case2
+            if(list.get(lp)+list.get(rp)<target)
+            {
+                 lp++;
+            }    
+            //case3
+            else
+            { 
+                rp--;
+            }    
+
+        }
+        return false;
+    }
+
+    public static void main (String args[])
+    {
+        ArrayList<Integer> list= new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        int target=50;
+        System.out.println(pairsum1(list,target));
+    }
+}
