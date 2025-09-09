9ca3b169295acbebf79278c9a440c1106528457f
Sneha Mandal <144563264+snehamandal18@users.noreply.github.com>
2025-09-09 09:29:12 +0530

    Add files via upload

---
+++ java vs file/UnionIntersection.java
@@ -0,0 +1,38 @@
+
+import java.util.*;
+public class UnionIntersection{
+    public static void main(String args[])
+    {
+        int arr1[]={7,3,9};
+        int arr2[]={6,3,9,2,9,4};
+        HashSet<Integer> set = new HashSet<>();
+        //union
+        for(int i=0;i<arr1.length;i++)
+        {
+           set.add(arr1[i]);
+        }
+        for(int i=0;i<arr2.length;i++)
+        {
+           set.add(arr2[i]);
+        }
+        System.out.println("Union:"+set.size());
+
+        //intersection
+        set.clear();
+        int cnt=0;
+        for(int i=0;i<arr1.length;i++)
+        {
+           set.add(arr1[i]);
+        }
+        for(int i=0;i<arr2.length;i++)
+        {
+           if(set.contains(arr2[i]))
+           {
+              cnt++;
+              set.remove(arr2[i]);
+           }
+        }
+        System.out.println("Intersection:"+cnt);
+
+    }
+}
