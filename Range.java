ab3106cf05d3ad4a1d4d6ca5fa612c83efbcbfb1
Sneha Mandal <144563264+snehamandal18@users.noreply.github.com>
2026-02-19 23:40:44 +0530

    Add files via upload

---
+++ Range.java
@@ -0,0 +1,80 @@
+
+
+public class Range {
+     static class Node {
+        int data ;
+        Node left ;
+        Node right;
+        Node (int data)
+        {
+            this .data = data;
+        }
+    }
+    // insertion
+    public static Node insert (Node root , int val)
+    {
+           if(root == null)
+           {
+            root = new Node (val);
+            return root;
+           }
+           if(root.data > val)
+           {
+            //left subtree 
+            root.left = insert (root.left,val);
+           }
+           else
+           {
+            //right subtree 
+            root.right = insert (root.right,val);
+           }
+          return root; 
+    }
+    //inorder traversal
+    public static void inorder (Node root)
+    {  
+        //base case
+        if(root == null) return ;
+
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    } 
+
+    // print in range
+    public static void printInRange(Node root , int k1 , int k2)
+    {
+        // base case 
+        if(root == null)
+        {
+            return ;
+        }
+        if(root.data >= k1 && root.data <= k2)
+        {
+            printInRange(root.left, k1, k2);
+            System.out.print(root.data + " ");
+            printInRange(root.right, k1, k2);
+        }
+        else if(root.data < k1)
+        {
+            printInRange(root.left, k1, k2);
+        }
+        else 
+        {
+          printInRange(root.right, k1, k2);
+        }
+    } 
+
+    public static void main(String args[])
+    {
+        int values[] = {8,5,3,1,4,6,10,11,14};
+        Node root = null;
+        for(int i = 0 ; i<values.length ; i++)
+        {
+            root = insert (root , values[i]);
+        }
+       inorder(root);
+       System.out.println();
+       printInRange(root, 5, 12);
+    }
+}
