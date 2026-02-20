f552cf1bfb3c012db55eb29960536bca69b3c046
Sneha Mandal <144563264+snehamandal18@users.noreply.github.com>
2026-02-20 09:24:43 +0530

    Add files via upload

---
+++ SortedArr2BalancedBST.java
@@ -0,0 +1,50 @@
+public class SortedArr2BalancedBST {
+    static class Node {
+        int data ;
+        Node left ;
+        Node right;
+        Node (int data)
+        {
+            this .data = data;
+        }
+    }
+    public static void preorder(Node root)
+    {
+        if(root == null)
+        {
+            return ;
+        }
+        System.out.print(root.data +" ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    // create balanced bst
+    public static Node createBST(int arr[] , int st , int end)
+    {
+        // base case
+        if(st>end)
+        {
+            return null;
+        }
+        int mid = (st+end)/2;
+        Node root = new Node(arr[mid]);
+        root.left = createBST(arr, st, mid-1);
+        root.right = createBST(arr, mid+1, end);
+        return root;
+
+    }
+    public static void main(String args[])
+    {
+        int values[] = {3,5,6,8,10,11,12};
+        /*
+                   8
+                 /  \
+                 5   11
+                / \  / \
+               3   6 10   12
+         */
+        Node root = createBST(values, 0, values.length-1);
+        preorder(root);
+    }
+}
